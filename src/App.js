39a14305509d970b541774fe4d49fa1560ee7e1e
Geir Gulbrandsen <geir.gulbrandsen@Administrators-MacBook-Pro.local>
2020-11-03 11:56:39 +0000

    Simple a11y experiment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const clickHandler = (() => {
+        const element = document.createElement("div")
+        element.textContent = 'Alert, Alert!'
+        document.getElementById('alert').append(element)
+    });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>This is an amazing test</p>
+                <p id="alert" role={'alert'}></p>
+                <button onClick={clickHandler}>Test</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
