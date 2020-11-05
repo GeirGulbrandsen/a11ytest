1f9dd5720f0abe914c7b9be5c6c5afc03216a636
Geir Gulbrandsen <geir.gulbrandsen@Administrators-MacBook-Pro.local>
2020-11-05 10:00:04 +0000

    eslint is now configured

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 
 function App() {
     const clickHandler = (() => {
-        const element = document.createElement("div")
+        const element = document.createElement('div')
         element.textContent = 'Alert, Alert!'
-        document.getElementById('alert').append(element)
+        element.setAttribute('role', 'alert')
+        document.getElementById('test').append(element)
     });
 
     return (
         <div className="App">
             <header className="App-header">
-                <p>This is an amazing test</p>
-                <p id="alert" role={'alert'}></p>
+                <p id={'test'}>This is an amazing test</p>
                 <button onClick={clickHandler}>Test</button>
             </header>
         </div>
